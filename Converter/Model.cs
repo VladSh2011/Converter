db772811408c65918c61818ac0c12d16446566ca
VladSh2011 <129531386+VladSh2011@users.noreply.github.com>
2024-04-05 19:01:38 +0300

    Add project files.

---
+++ Converter/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Converter
+{
+    class Model
+    {
+        public double CtoF(double x) => x * 9 / 5 + 32;
+    }
+}
---
+++ Converter/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using WpfApp2;
+using System.Windows.Input;
+
+namespace Converter
+{
+    class ViewModel : BaseVM
+    {
+        private Model model;
+        private RelayCommand _ConvertCommand;
+        private string _inputValue;
+        private string _outputValue;
+        private double _num;
+
+        public string InputValue
+        {
+            get => _inputValue;
+            set
+            {
+                _inputValue = value;
+                OnPropertyChanged(nameof(InputValue));
+            }
+        }
+        public string OutputValue
+        {
+            get => _outputValue;
+            set
+            {
+                _outputValue = value;
+                OnPropertyChanged(nameof(OutputValue));
+            }
+        }
+
+        public ViewModel()
+        {
+            model = new Model();
+            _ConvertCommand = new RelayCommand(GetNum);
+            OutputValue = _num.ToString();
+        }
+
+        public ICommand Command => _ConvertCommand;
+
+        public void GetNum(object o)
+        {
+            double.TryParse(InputValue, out double num);
+            model.CtoF(num);
+            _num = num;
+        }
+    }
+}
